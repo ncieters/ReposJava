5a5b41b3cf208c0f3f69dc7c8f6dcadf89d5f7f0
Nicolas Cieters <nicolas.cieters@outlook.fr>
2020-06-03 18:35:15 +0200

    test 1

---
+++ HelloWorld.java
@@ -0,0 +1,37 @@
+
+public class HelloWorld {
+	/** Le programme commence ici */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!1");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!2");
+        System.out.println("Hello World!2");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!23");
+        System.out.println("Hello World!2555");
+        System.out.println("Hello World!2666");
+        System.out.println("Hello World!277888");
+        System.out.println("Hello World!27788833333333");
+        
+        int t=2;
+        int bonusAllowance=500;
+        System.out.println(t); 
+       
+        //Déclarez la variable int ongoingAllowance
+        int ongoingAllowance = 200; 
+        //Déclarez la variable int savings
+        int savings = 3000;
+        
+        //TODO : Déclarez une variable int nommée bonusAllowance avec la valeur 500
+
+       
+        //Affiche le résultat
+        System.out.println("You have an ongoing allowance of " + ongoingAllowance +"\nYour savings amount is " + savings +"\nYou have a bonus allowance of " + bonusAllowance);
+        
+        
+        
+        
+        
+    }
+}
